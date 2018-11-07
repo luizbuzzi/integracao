bf4b16e55b8a791418a463f06d80fa33c291848c
luizbuzzi <luizbuzzi@gmail.com>
2018-11-07 15:30:04 -0200

    Create service.py

---
+++ service.py
@@ -0,0 +1,23 @@
+#!flask/bin/python
+from flask import Flask, abort, request
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route('/time', methods=['GET'])
+def time():
+    origin = request.args.get('origin')
+    destination = request.args.get('destination')
+    variable = request.args.get('variable')
+    payload = {'origins':origin,
+               'destinations':destination,
+               'key':'AIzaSyBb8sJ270Ka_6a3vH1g4CAEpeUxMVDVIhY'}
+    r = requests.get('https://maps.googleapis.com/maps/api/distancematrix/json',
+                    params=payload)
+    
+    return str(data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
